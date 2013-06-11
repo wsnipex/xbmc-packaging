4e65fb4c3fc86ba1e8da1e9f9ae47bd9952eabe8
wsnipex <wsnipex@a1.net>
2013-06-11 11:29:47 +0200

    [binary addons] add wrapper script for PPA build

---
+++ addons-ppa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CLEANUP_AFTER="True"
+TAG=${TAG:-"1"} \
+BRANCH="master" \
+DISTS="raring quantal precise" \
+BUILDER="debuild" \
+DEBUILD_OPTS="-S" \
+PPA_UPLOAD="True" \
+DPUT_TARGET="ppa:wsnipex/xbmc-nightly" \
+./build-xbmc-addons.sh
