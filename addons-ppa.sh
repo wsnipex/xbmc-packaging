be7a42cc4a626cc46eccaa7c36b0a8ffde763cc3
wsnipex <wsnipex@a1.net>
2013-06-21 15:44:18 +0200

    adjust to PPA list

--- addons-ppa.sh
+++ addons-ppa.sh
@@ -4,8 +4,11 @@ CLEANUP_AFTER="True" \
 TAG=${TAG:-"1"} \
 BRANCH="master" \
 DISTS="raring quantal precise" \
-BUILDER="debuild" \
-DEBUILD_OPTS="-S" \
 PPA_UPLOAD="True" \
-DPUT_TARGET="ppa:wsnipex/xbmc-nightly" \
+PPA="wsnipex-nightly" \
 ./build-xbmc-addons.sh
+
+#BUILDER="debuild" \
+#DEBUILD_OPTS="-S" \
+#DPUT_TARGET="ppa:wsnipex/xbmc-nightly" \
+
