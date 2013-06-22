fde98c84c33235844dc3bff8962361f28cba2adb
wsnipex <wsnipex@a1.net>
2013-06-22 12:01:53 +0200

    [update-addon-repo.sh] add cleanup

--- update-addon-repo.sh
+++ update-addon-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PPA=${PPA:-"stable"}
+PPA=${PPA:-"auto"}
 DISTS=${DISTS:-"raring quantal precise"}
 ARCHS="i386 amd64"
 WORK_DIR=${WORKSPACE:-$(pwd)}
@@ -24,7 +24,6 @@ declare -A PPAPACKAGES_i386
 declare -A PPAPACKAGES_amd64
 
 function checkPpaVersion {
-    
     for arch in $ARCHS
     do
         PACKAGES=$(echo ${PPAS["$PPA"]} | sed -e "s/#dist#/$DIST/" -e "s/#arch#/$arch/")
@@ -37,12 +36,12 @@ function checkPpaVersion {
 }
 
 function verifyBuild {
-
-    cd $WATCH || exit 1
+    cd $WATCH/$PPA || exit 1
     for DIST in $DISTS
     do
         checkPpaVersion
-        echo ; echo "checking uploads in dist: $DIST"
+        echo ; echo "checking uploads in PPA: $PPA dist: $DIST"
+        [ -r ${DIST}.addon.list ] || continue
         while read PACKAGE VERSION
         do
             echo -n "Package: $PACKAGE Version: $VERSION PPA_VERSION: ${PPAPACKAGES_i386["$PACKAGE"]:-"none"}"
@@ -77,6 +76,8 @@ function removeFromList {
     sed -i "/^$PACKAGE.*/d" ${DIST}.addon.list
     #rm $DIST/$zipname
     unset zipname
+    local filesize=$(wc -l ${DIST}.addon.list)
+    [ ${filesize:0:1} -eq 0 ] && rm ${DIST}.addon.list
 }
 
 function checkSuperseded {
@@ -88,6 +89,29 @@ function checkSuperseded {
             echo "Version in PPA greater then ${VERSION}, removing superseded package from list"
             removeFromList
         fi
+    else
+        echo "Error: package has different versions in i386 and amd64"
+        echo "i386: ${PPAPACKAGES_i386["$PACKAGE"]} amd64: ${PPAPACKAGES_amd64["$PACKAGE"]}"
+    fi
+}
+
+function cleanup {
+    cd $WATCH || exit 1
+    if [ -d $PPA ]
+    then
+        local addonfiles=$(ls $PPA/*.addon.list 2>/dev/null)
+        if [ -z "$addonfiles" ]
+        then
+            echo "All Addons in $PPA successfully processed, removing directory"
+            rm -rf $PPA
+        fi
+    fi
+
+    local ppadirs=$(ls -d $WATCH/* 2>/dev/null)
+    if [ -z "$ppadirs" ]
+    then
+        echo "All PPAs successfully processed, removing watch dir"
+        cd .. && rm -rf $WATCH
     fi
 }
 
@@ -100,4 +124,17 @@ then
     exit 0
 fi
 
-verifyBuild
+if [[ "$PPA" == "auto" ]]
+then
+    for PPA in ${!PPAS[*]}
+    do
+        [[ -d $WATCH/$PPA ]] && verifyBuild || continue
+        cleanup
+    done
+    cleanup
+else
+    verifyBuild
+    cleanup
+fi
+
+
