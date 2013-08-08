7d58889a8e919e386c0d55e85ec9b08ee7c9f55b
wsnipex <wsnipex@a1.net>
2013-08-08 07:43:40 +0200

    [addons-ppa.sh] allow override of dists

--- addons-ppa.sh
+++ addons-ppa.sh
@@ -4,7 +4,7 @@
 CLEANUP_AFTER="True" \
 TAG=${TAG:-"1"} \
 BRANCH=${BRANCH:-"master"} \
-DISTS="saucy raring quantal precise" \
+DISTS=${DIST:-"saucy raring quantal precise"} \
 PPA_UPLOAD="True" \
 PPA=${PPA:-"wsnipex-xbmc-addons-test"} \
 URGENCY=${URGENCY:-"low"} \
